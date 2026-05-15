9da1e12c03a2df56d74354cf1c537d9e86e9fb4d
NicolasPerezU <u20221204357@usco.edu.co>
2026-05-15 15:47:24 -0500

    hora local

--- src/main/java/com/SIGMA/USCO/SigmaApplication.java
+++ src/main/java/com/SIGMA/USCO/SigmaApplication.java
@@ -1,13 +1,21 @@
 package com.SIGMA.USCO;
 
 import com.SIGMA.USCO.config.EnvLoader;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class SigmaApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+    }
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SigmaApplication.class);
